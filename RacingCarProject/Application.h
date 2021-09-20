e8547d2d9b860d43b7368a31753576e3d107a315
Ruizo <marc_ruiz1995@hotmail.com>
2021-09-20 09:16:44 +0200

    deleted vehicle and things
    Co-Authored-By: Ignasi Pardo Carbó <60847464+kuronoascarlet@users.noreply.github.com>

--- RacingCarProject/Application.h
+++ RacingCarProject/Application.h
@@ -11,7 +11,7 @@
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
 #include "ModulePhysics3D.h"
-#include "ModulePlayer.h"
+
 
 class Application
 {
@@ -24,7 +24,7 @@ public:
 	ModuleRenderer3D* renderer3D;
 	ModuleCamera3D* camera;
 	ModulePhysics3D* physics;
-	ModulePlayer* player;
+
 
 private:
 
