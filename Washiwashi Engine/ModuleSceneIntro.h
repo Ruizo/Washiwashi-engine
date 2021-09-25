984908b9234ce4f6f99df890930e2afa12c0eabf
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2021-09-25 23:46:55 +0200

    Organized some folders and linked some libraries

--- Washiwashi Engine/ModuleSceneIntro.h
+++ Washiwashi Engine/ModuleSceneIntro.h
@@ -2,9 +2,9 @@
 #include "Module.h"
 #include "p2DynArray.h"
 #include "Globals.h"
-#include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_sdl.h"
-#include "imgui/backends/imgui_impl_opengl3.h"
+#include "External/imgui/imgui.h"
+#include "External/imgui/backends/imgui_impl_sdl.h"
+#include "External/imgui/backends/imgui_impl_opengl3.h"
 #include <gl/GL.h>
 
 class ModuleSceneIntro : public Module
