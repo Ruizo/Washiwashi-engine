ca33a9c024a3d03ccc38e78f6305219050bc52d3
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2021-10-07 17:36:14 +0200

    PhysFS included.
    Assimp integration on progress

--- Washiwashi Engine/Application.h
+++ Washiwashi Engine/Application.h
@@ -10,6 +10,7 @@
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
 #include "ModuleEditor.h"
+#include "ModuleFileSystem.h"
 #include "External/Parson/parson.h"
 #include "External/MathGeoLib/include/MathGeoLib.h"
 
