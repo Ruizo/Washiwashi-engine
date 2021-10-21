581562ed3cd89f8e06dbc3a9483655e6ad776c04
Ruizo <marc_ruiz1995@hotmail.com>
2021-10-21 17:43:04 +0200

    Baker house loaded

--- Washiwashi Engine/ModuleSceneIntro.cpp
+++ Washiwashi Engine/ModuleSceneIntro.cpp
@@ -19,7 +19,7 @@ bool ModuleSceneIntro::Start()
 
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
-	warrior.LoadMesh("Engine/Library/Meshes/warrior.FBX");
+	warrior.LoadMesh("Engine/Library/Meshes/BakerHouse.FBX");
 	return ret;
 }
 
