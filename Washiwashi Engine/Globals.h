4155e6745a5dd56e1732654bae18c82c3a24547e
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2021-10-23 19:47:32 +0200

    Update Globals.h

--- Washiwashi Engine/Globals.h
+++ Washiwashi Engine/Globals.h
@@ -20,7 +20,6 @@
 #define SHADERS_PATH "Engine/Library/Shaders/"
 #define SCENES_PATH "Engine/Library/Scenes/"
 
-
 void log(const char file[], int line, const char* format, ...);
 
 #define CAP(n) ((n <= 0.0f) ? n=0.0f : (n >= 1.0f) ? n=1.0f : n=n)
@@ -31,7 +30,6 @@ void log(const char file[], int line, const char* format, ...);
 #define DEGTORAD 0.0174532925199432957f
 #define RADTODEG 57.295779513082320876f
 #define HAVE_M_PI
-
 
 typedef unsigned int uint;
 
