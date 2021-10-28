8ceedd7edaf04e6fe06a10d5ab9963a0e8a10e32
Ruizo <marc_ruiz1995@hotmail.com>
2021-10-28 18:19:12 +0200

    merged done bad, fixed rotation now

--- Washiwashi Engine/ComponentTransform.cpp
+++ Washiwashi Engine/ComponentTransform.cpp
@@ -29,8 +29,22 @@ void ComponentTransform::Update()
 {
 	if (active)
 	{
-		if(updateTransform)
-		UpdateTransform();
+		if (updateTransform)
+		{
+			UpdateTransform();
+		}
+		if (rotation.x)
+		{
+			setRotationX();
+		}
+		if (rotation.y)
+		{
+			setRotationY();
+		}
+		if (rotation.z)
+		{
+			setRotationZ();
+		}
 	}
 
 }
@@ -40,7 +54,10 @@ void ComponentTransform::Draw()
 	if (ImGui::CollapsingHeader("Local Transformation"))
 	{
 		if (ImGui::SliderFloat3("Position", &position, -200, 200)) updateTransform = true;
-		if (ImGui::SliderFloat3("Rotation", &rotation, -180, 180)) updateTransform = true;
+		if (ImGui::DragFloat("Rotation X", &rotation.x, -20, 20)) rotationx = true;
+		if (ImGui::DragFloat("Rotation Y", &rotation.y, -20, 20)) rotationy = true;
+		if (ImGui::DragFloat("Rotation Z", &rotation.z, -20, 20)) rotationz = true;
+		//if (ImGui::SliderFloat3("Rotation", &rotation, -180, 180)) updateTransform = true;
 		if (ImGui::SliderFloat3("Scale", &scale, 0, 100)) updateTransform = true;
 	}
 }
@@ -50,13 +67,32 @@ void ComponentTransform::UpdateTransform()
 
 	SetPos(position.x, position.y, position.z);
 
-	SetRotation(rotation.x, vec3(1, 0, 0));
-	SetRotation(rotation.y, vec3(0, 1, 0));
-	SetRotation(rotation.z, vec3(0, 0, 1));
+	//SetRotation(rotation.x, vec3(1, 0, 0));
 
 	Scale(scale.x, scale.y, scale.z);
 
 	updateTransform = false;
+}
+
+void ComponentTransform::setRotationX()
+{
+	SetRotation(rotation.x, vec3(1, 0, 0));
+
+	rotationx = false;
+}
+
+void ComponentTransform::setRotationY()
+{
+	SetRotation(rotation.y, vec3(0, 1, 0));
+
+	rotationy = false;
+}
+
+void ComponentTransform::setRotationZ()
+{
+	SetRotation(rotation.z, vec3(0, 0, 1));
+
+	rotationz = false;
 }
 
 void ComponentTransform::SetPos(float x, float y, float z)
